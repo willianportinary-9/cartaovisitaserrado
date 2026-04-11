54341478b1eb02fc2dd2a969b32650c832d01e5e
willianportinary-9 <willianportinary@gmail.com>
2026-04-11 19:41:12 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -8,3 +8,24 @@ function buscar() {
     card.style.display = texto.includes(input) ? "block" : "none";
   });
 }
+function abrirModal(img) {
+  const modal = document.getElementById("modal");
+  const imgModal = document.getElementById("imgModal");
+
+  modal.style.display = "flex"; 
+  imgModal.src = img.src;
+}
+
+// FECHAR MODAL
+function fecharModal() {
+  document.getElementById("modal").style.display = "none";
+}
+
+// FECHAR AO CLICAR FORA DA IMAGEM
+const modal = document.getElementById("modal");
+
+modal.addEventListener("click", function(e) {
+  if (e.target === modal) {
+    fecharModal();
+  }
+});
